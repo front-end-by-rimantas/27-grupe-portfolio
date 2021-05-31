e37c745a0848a309265edfde011c1e95eba29d4a
Rimantas <belauzas@yahoo.com>
2021-05-31 12:42:49 +0300

    More features test without validations

--- js/components/features/Features.js
+++ js/components/features/Features.js
@@ -4,6 +4,21 @@ class Features {
         this.data = data;
 
         this.DOM = null;
+        this.maxItemsPerList = 'all';       // default: all, allowed: any postivie integer
+        this.allowedRenderStrategies = ['first', 'last', 'random'];
+        this.listRenderStrategy = this.allowedRenderStrategies[0];
+
+        this.init();
+    }
+
+    init() {
+        if (!this.isValidSelector() ||
+            !this.findElementBySelector() ||
+            !this.isValidData()) {
+            return false;
+        }
+
+        this.render();
     }
 
     isValidSelector() {
@@ -14,16 +29,29 @@ class Features {
         return true;
     }
 
-    isValidData() {
-        return true;
-    }
-
     findElementBySelector() {
         const DOM = document.querySelector(this.selector);
         if (!DOM) {
             return false;
         }
         this.DOM = DOM;
+        return true;
+    }
+
+    isValidData() {
+        return true;
+    }
+
+    render() {
+        let HTML = '';
+
+        for (const feature of this.data.list) {
+            HTML += `<div class="col-12 col-md-6 col-lg-4">
+                        FEATURE 1
+                    </div>`;
+        }
+
+        this.DOM.innerHTML = HTML;
     }
 }
 
--- js/home.js
+++ js/home.js
@@ -1,3 +1,6 @@
 // IMPORT
+import { Features } from './components/features/Features.js';
+import { featuresData } from './data/featuresData.js';
 
 // EXECUTION
+new Features('#features_block', featuresData);
